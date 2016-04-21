424506b756a26d9f12e2f0fff2ea7288caf8af02
CEINO TECHNOLOGY <ceinotechnology@avinash.local>
2016-04-21 17:03:16 +0530

    Initial Commit

---
+++ ProjectApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectApp
+//
+//  Created by Ceino on 21/04/16.
+//  Copyright © 2016 CEINO TECHNOLOGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProjectApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ProjectApp
+//
+//  Created by Ceino on 21/04/16.
+//  Copyright © 2016 CEINO TECHNOLOGY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
